b0382d7103e6b41ea10a3746c732bec4d8fafae5
cgroll <groll.christian.edu@gmail.com>
2015-09-29 16:20:04 +0200

    first pic added as test

---
+++ src/missing_observations.jl
@@ -0,0 +1,24 @@
+## load required packages
+##-----------------------
+
+using TimeData
+loadPlotting()
+using Gadfly
+
+## load raw data
+##--------------
+
+pricesTm = readTimedata("financial_data/raw_data/SP500.csv")
+nObs, nAss = size(pricesTm)
+
+## get observations per stock
+##---------------------------
+
+obsPerStock = collapseDates(x -> sum(!isna(x)), eachcol(pricesTm))
+
+nObsHist = plot(x=convert(Array, obsPerStock), Geom.histogram(bincount = 20),
+                Scale.x_continuous(format = :plain),
+                Guide.xlabel("# observations"),
+                Guide.ylabel("# assets"))
+
+draw(SVG("pics/missing_observations-1.svg", 12cm, 12cm), nObsHist)
